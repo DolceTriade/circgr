f3b50a4e28f1d4902e1c5dddc78b26c8c58e86b8
dolcetriade <vcelestialragev@gmail.com>
2019-02-20 15:44:59 -0800

    Implement computing trace path length and centroid.

--- src/circgr.rs
+++ src/circgr.rs
@@ -15,9 +15,10 @@ pub enum Direction {
     Out,
     Clockwise,
     CounterClockwise,
+    MAX_DIRECTIONS,
 }
 
-#[derive(Default, Builder, Debug, Clone)]
+#[derive(Default, Builder, Debug, Clone, PartialEq)]
 #[builder(setter(into))]
 pub struct Point {
     x: f64,
@@ -56,6 +57,7 @@ pub struct Trace {
     resultant: ResultantVector,
 }
 
+#[derive(Default, Debug, Clone)]
 pub struct Gesture {
     traces: HashMap<u32, Trace>,
     anchors: HashMap<u32, Point>,
@@ -63,4 +65,57 @@ pub struct Gesture {
     resultant: ResultantVector,
     temporal_events: TemporalEvents,
     directional_events: DirectionalEvents,
+}
+
+impl Gesture {
+    fn new(raw_traces: &HashMap<u32, Vec<Point>>) -> Self {
+        build_gesture(&raw_traces)
+    }
+}
+
+fn build_gesture(raw_traces: &HashMap<u32, Vec<Point>>) -> Gesture {
+    let mut gesture = Gesture::default();
+
+    for (id, points) in raw_traces {
+        let info = compute_centroid_and_path_length(&points);
+    }
+
+    gesture
+}
+
+fn distance(a: &Point, b: &Point) -> f64 {
+    ((a.x - b.x).powf(2.0) + (a.y - b.y).powf(2.0)).sqrt()
+}
+
+#[derive(Default, Debug, Clone)]
+struct TraceInfo {
+    path_length: f64,
+    centroid: Point,
+    start_time: u32,
+    end_time: u32,
+}
+
+fn compute_centroid_and_path_length(points: &Vec<Point>) -> TraceInfo {
+    let mut info = TraceInfo::default();
+    info.start_time = points[0].timestamp;
+    info.end_time = points[0].timestamp;
+    info.centroid = points[0].clone();
+
+    for i in 1..points.len() {
+        let prev = &points[i - 1];
+        let p = &points[i];
+
+        info.path_length += distance(p, prev);
+        info.centroid.x += p.x;
+        info.centroid.y += p.y;
+
+        if p.timestamp < info.start_time {
+            info.start_time = p.timestamp;
+        }
+
+        if p.timestamp > info.end_time {
+            info.end_time = p.timestamp;
+        }
+    }
+    info
 }
