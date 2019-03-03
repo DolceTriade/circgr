103f175e4c733fdfc4ed96c4baf08f6e311b13a1
dolcetriade <vcelestialragev@gmail.com>
2019-03-02 17:28:06 -0800

    Add a simpler classifier to see if it makes detection better
    This only compares observations between the two gestures and
    doesn't compare them based on direction.

--- src/classifier.rs
+++ src/classifier.rs
@@ -21,6 +21,10 @@ impl Classifier {
         classify_impl(&gesture, &self.templates[..])
     }
 
+    pub fn classify_dumb(&self, gesture: &Gesture) -> Option<(String, f64)> {
+        classify_dumb_impl(&gesture, &self.templates[..])
+    }
+
     pub fn add_template(&mut self, template: Gesture) {
         self.templates.push(template);
     }
@@ -127,4 +131,50 @@ fn all_directions(a: &Gesture, b: &Gesture) -> HashSet<Direction> {
 
 fn observation_distance(a: f64, b: f64) -> f64 {
     PI - (PI - (a - b).abs()).abs()
+}
+
+fn classify_dumb_impl(gesture: &Gesture, templates: &[Gesture]) -> Option<(String, f64)> {
+    let mut min = std::f64::MAX;
+    let mut name = "".to_string();
+    let co = sorted_observations(&gesture);
+    for template in templates {
+        if template.anchors.len() != gesture.anchors.len()
+            || template.traces.len() != gesture.traces.len()
+        {
+            continue;
+        }
+
+        let mut distance = 0.0_f64;
+        let to = sorted_observations(template);
+        let min_len = co.len().min(to.len());
+        let sum: f64 = izip!(&co[..min_len], &to[..min_len])
+            .map(|x| -> f64 { observation_distance(*x.0, *x.1) })
+            .sum();
+        distance += sum;
+        distance += match co.len() {
+            _ if co.len() == min_len => &to[min_len..].iter().sum() + 0.0_f64,
+            _ => &co[min_len..].iter().sum() + 0.0_f64,
+        };
+        if min > distance {
+            name = template.name.clone();
+            min = distance;
+        }
+    }
+
+    if name.is_empty() {
+        return None;
+    }
+
+    return Some((name, min));
+}
+
+fn sorted_observations(gesture: &Gesture) -> Vec<f64> {
+    let mut v: Vec<f64> = gesture
+        .traces
+        .values()
+        .flat_map(|s| s.observations.iter())
+        .cloned()
+        .collect();
+    v.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+    v
 }
